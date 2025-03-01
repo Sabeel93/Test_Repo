e2e04c24b3fc08bbc4ea44cb60f9872fdebfed33
Sabeel93 <sabeelshakeel@hotmail.co.uk>
2025-03-01 19:08:31 +0000

    Update API.py
    added post request to add new product

--- API.py
+++ API.py
@@ -1,5 +1,7 @@
 import requests
 
-response = requests.get("https://fakestoreapi.com/products")
+#response = requests.get("https://fakestoreapi.com/products")
+product = {'title': 'New Product', 'price': 29.99, 'descrition': 'this is a new product', 'category': 'unknown'}
+response = requests.post('https://fakestoreapi.com/products', json=product)
 
 print(response.json())
