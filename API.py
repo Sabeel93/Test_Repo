fde32d86e59bd252abccf211ab66d6cd0fd7614d
Sabeel93 <sabeelshakeel@hotmail.co.uk>
2025-03-01 18:47:09 +0000

    Update API.py
    revert

--- API.py
+++ API.py
@@ -1,9 +1,5 @@
-#import requests
+import requests
 
-#response = requests.get("https://fakestoreapi.com/products")
+response = requests.get("https://fakestoreapi.com/products")
 
-#print(response.json())
-
-fetch('https://fakestoreapi.com/products/1')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
+print(response.json())
